b317728d7622e667e5041ebf79b161e835c0d293
Joe Wass <joe@afandian.com>
2015-01-08 10:15:50 +0000

    Moved disk-persisted parsed lines to central location to prevent re-parsing multiple times.

--- src/laskuri/core.clj
+++ src/laskuri/core.clj
@@ -61,18 +61,7 @@
   (f/reduce-by-key
     (f/map collection (f/fn [[k _]] [k 1]))
     (f/fn [a b] (+ a b))))
-  
-; (defn sort-by-selector
-;   "For a key value collection, order by the selector (which acts on the kv pair) and then return in original format."
-;   [collection selector]
-;   ; (f/map
-;     ; Sort by the key
-;     ; (f/sort-by-key
-;       ; Map into [selected field, kv pair]
-;       (f/map collection (f/fn [kv] [(selector kv) kv])))
-;     ; Strip key and return to original format.
-;     ; (f/fn [k v] v)))
-    
+
 (defn sort-by-selector
   "For a key value collection, order by the selector (which acts on the kv pair) and then return in original format."
   [collection sel] 
@@ -84,28 +73,21 @@
 
 (defn generate-all-time
   "Generate figures for all-time."
-  [ctx input-location output-location redact]
-  (let [parsed-lines (get-parsed-lines ctx input-location redact)
-        
-        ; filter out lines that didn't resolve, leaving good DOIs
-        parsed-lines-ok (f/persist
-                          (f/filter parsed-lines (f/fn [[date doi domain status]] (not= 0 (.length status))))
-                          StorageLevels/DISK_ONLY)
-        
-        ; doi -> date
-        doi-date (f/map parsed-lines-ok (f/fn [[date doi [subdomain domain tld] status]]
+  [ctx input-location output-location redact parsed-lines]
+  (let [; doi -> date
+        doi-date (f/map parsed-lines (f/fn [[date doi [subdomain domain tld] status]]
                                             [doi date]))
         
         ; domain -> date
-        domain-date (f/map parsed-lines-ok (f/fn [[date doi [subdomain domain tld] status]]
+        domain-date (f/map parsed-lines (f/fn [[date doi [subdomain domain tld] status]]
                                               [(str domain "." tld) date]))
 
         ; [domain with subdomain, domain] -> doi
-        subdomain-doi (f/map parsed-lines-ok (f/fn [[date doi [subdomain domain tld] status]]
+        subdomain-doi (f/map parsed-lines (f/fn [[date doi [subdomain domain tld] status]]
                                                 [[(str subdomain "." domain "." tld) domain] doi]))
         
         ; domain -> doi
-        domain-doi (f/map parsed-lines-ok (f/fn [[date doi [subdomain domain tld] status]]
+        domain-doi (f/map parsed-lines (f/fn [[date doi [subdomain domain tld] status]]
                                              [(str domain "." tld) doi]))
               
         ;; Outputs
@@ -122,8 +104,6 @@
         ; domain and subdomain, domain -> count
         ; including both subdomain and domain is necessary for the consumer of this dataset.
         subdomain-count (count-by-key-sorted subdomain-doi)]
-      
-      
     (.saveAsTextFile (f/map doi-first-date format-kv) (str output-location "/ever-doi-first-date"))
     (.saveAsTextFile (f/map doi-count format-kv) (str output-location "/ever-doi-count"))
     (.saveAsTextFile (f/map domain-count format-kv) (str output-location "/ever-domain-count"))
@@ -131,17 +111,12 @@
 
 (defn generate-per-period
   "Generate figures per-day."
-  [ctx period input-location output-location redact]
+  [ctx period input-location output-location redact parsed-lines]
   {:pre [(#{:year :month :day nil} period)]}
-  (let [parsed-lines (get-parsed-lines ctx input-location redact)
-        
-        ; filter out lines that didn't resolve, leaving good DOIs
-        parsed-lines-ok (f/filter parsed-lines (f/fn [[date doi domain status]] (not= 0 (.length status))))
-        
-        ; date truncated to period
+  (let [; date truncated to period
         ; date represents the beginning of the period (i.e. first second of the day, month or year).
         parsed-lines-period (f/persist 
-                              (f/map parsed-lines-ok (f/fn [[date doi domain status]]
+                              (f/map parsed-lines (f/fn [[date doi domain status]]
                                 [(condp = period
                                  :year (util/truncate-year date)
                                  :month (util/truncate-month date)
@@ -197,7 +172,6 @@
     (.saveAsTextFile (f/map domain-period-count format-ksv) (str output-location "/" (name period) "-domain-period-count"))
     (.saveAsTextFile (f/map subdomain-period-count format-ksv) (str output-location "/" (name period) "-subdomain-period-count"))))
 
-
 (defn -main
   [& args]
   (let [input-location (env :input-location)
@@ -214,22 +188,27 @@
                    (conf/set "spark.executor.memory" "500m")
                    (conf/set "spark.kryoserializer.buffer.mb" "256"))
                 (conf/spark-conf))
-          sc (f/spark-context conf)]  
+          sc (f/spark-context conf)
+          
+          parsed-lines (get-parsed-lines sc input-location redact)
+        
+          ; filter out lines that didn't resolve, leaving good DOIs
+          parsed-lines-ok (f/filter parsed-lines (f/fn [[date doi domain status]] (not= 0 (.length status))))]  
     (when (and input-location output-location)
       (info "Input" input-location)
       (info "Output" output-location)   
       (when (= (.toLowerCase (or (env :alltime) "false")) "true")
         (info "generate-all-time")
-        (generate-all-time sc input-location output-location redact))
+        (generate-all-time sc input-location output-location redact parsed-lines-ok))
       
       (when (= (.toLowerCase (or (env :year) "false")) "true")
         (info "per year")
-        (generate-per-period sc :year input-location output-location redact))
+        (generate-per-period sc :year input-location output-location redact parsed-lines-ok))
       
       (when (= (.toLowerCase (or (env :month) "false")) "true")
         (info "per month")
-        (generate-per-period sc :month input-location output-location redact))
+        (generate-per-period sc :month input-location output-location redact parsed-lines-ok))
       
       (when (= (.toLowerCase (or (env :day) "false")) "true")
         (info "per day")
-        (generate-per-period sc :day input-location output-location redact)))))
+        (generate-per-period sc :day input-location output-location redact parsed-lines-ok)))))
